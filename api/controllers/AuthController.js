ee1b9039a9a376c3f93325e29e8ce05e2017c405
Guillaume Viguier <guillaume@viguierjust.com>
2016-11-09 17:23:10 +0100

    Oauth2 integration

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -190,7 +190,7 @@ module.exports = class AuthController extends Controller{
   accessTokenOauth2 (request, reply) {
     const oauth = this.app.packs.hapi.server.plugins['hapi-oauth2orize']
     request.auth.credentials = request.params.currentUser
-    oauth.authorize(function (clientID, redirect, done) {
+    oauth.authorize(request, reply, function (clientID, redirect, done) {
       console.log(clientID);
       console.log(redirect);
       done(null, clientID, redirect);
