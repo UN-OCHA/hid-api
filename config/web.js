06c5ce3ce665b5caf625bd9ad90ee1e227db668d
Guillaume Viguier <guillaume@viguierjust.com>
2016-11-09 17:52:00 +0100

    Oauth2 integration

--- config/web.js
+++ config/web.js
@@ -90,6 +90,8 @@ module.exports = {
         .findOne({token: code, type: 'code'})
         .populate('client user')
         .exec(function (err, ocode) {
+          console.log(code);
+          console.log(ocode);
           if (err || ocode.client._id !== client._id || redirectURI !== ocode.client.redirectUri) {
             return done(null, false);
           }
