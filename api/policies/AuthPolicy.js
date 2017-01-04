46db2d82b583ca9a70e0b5cc88f2047178bb6d3a
Guillaume Viguier <guillaume@viguierjust.com>
2017-01-04 11:31:44 +0100

    Fixed issue with token id

--- api/policies/AuthPolicy.js
+++ api/policies/AuthPolicy.js
@@ -1,7 +1,8 @@
-'use strict'
+'use strict';
 
-const Policy = require('trails-policy')
+const Policy = require('trails-policy');
 const Boom = require('boom');
+const Schema = require('mongoose').Schema;
 
 /**
  * @module AuthPolicy
@@ -10,7 +11,7 @@ const Boom = require('boom');
 module.exports = class AuthPolicy extends Policy {
 
   isAuthenticated (request, reply) {
-    const OauthToken = this.app.orm['OauthToken']
+    const OauthToken = this.app.orm.OauthToken;
     // If we are creating a user and we are not authenticated, allow it
     if (request.path == '/api/v2/user' && request.method == 'post' && !request.headers.authorization && !request.params.token) {
       return reply();
@@ -54,19 +55,22 @@ module.exports = class AuthPolicy extends Policy {
           .populate('user client')
           .exec(function (err, tok) {
             // TODO: make sure the token is not expired
-            if (err || !tok) return reply(Boom.unauthorized('Invalid Token!'));
-            request.params.currentUser = tok.user
-            reply()
+            if (err || !tok) {
+              return reply(Boom.unauthorized('Invalid Token!'));
+            }
+            request.params.currentUser = tok.user;
+            reply();
           });
       }
       else {
         request.params.token = jtoken; // This is the decrypted token or the payload you provided
-        that.app.orm['user'].findOne({_id: jtoken.id}, function (err, user) {
+        that.app.orm.User.findOne({_id: Schema.ObjectId(jtoken.id)}, function (err, user) {
           if (!err && user) {
             request.params.currentUser = user;
             reply();
           }
           else {
+            that.log.error(err);
             reply(Boom.unauthorized('Invalid Token!'));
           }
         });
@@ -76,22 +80,33 @@ module.exports = class AuthPolicy extends Policy {
 
   isAdmin (request, reply) {
     this.isAuthenticated(request, function (err) {
-      if (err && err.isBoom) return reply(err)
-      if (!request.params.currentUser) return reply(Boom.unauthorized('Current user was not set'))
-      if (!request.params.currentUser.is_admin) return reply(Boom.forbidden('You need to be an admin'))
+      if (err && err.isBoom) {
+        return reply(err);
+      }
+      if (!request.params.currentUser) {
+        return reply(Boom.unauthorized('Current user was not set'));
+      }
+      if (!request.params.currentUser.is_admin) {
+        return reply(Boom.forbidden('You need to be an admin'));
+      }
       reply()
-    })
+    });
   }
 
   isAdminOrCurrent (request, reply) {
     this.isAuthenticated(request, function (err) {
-      if (err && err.isBoom) return reply(err)
-      if (!request.params.currentUser) return reply(Boom.unauthorized('Current user was not set'))
-      if (!request.params.currentUser.is_admin && request.params.currentUser.id != request.params.id) return reply(Boom.unauthorized('You need to be an admin or the current user'))
-      reply()
-    })
+      if (err && err.isBoom) {
+        return reply(err);
+      }
+      if (!request.params.currentUser) {
+        return reply(Boom.unauthorized('Current user was not set'));
+      }
+      if (!request.params.currentUser.is_admin && request.params.currentUser.id != request.params.id) {
+        return reply(Boom.unauthorized('You need to be an admin or the current user'));
+      }
+      reply();
+    });
   }
 
 
-}
-
+};
