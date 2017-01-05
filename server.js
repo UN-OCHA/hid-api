90dda4cd8f703a8311bc3ecd800f633504bbbe8d
Guillaume Viguier <guillaume@viguierjust.com>
2017-01-05 14:07:30 +0100

    Handling errors differently

--- server.js
+++ server.js
@@ -4,10 +4,15 @@
  * Start up the Trails Application.
  */
 
-'use strict'
+'use strict';
 
-const app = require('./')
-const TrailsApp = require('trails')
-const server = new TrailsApp(app)
+const app = require('./');
+const TrailsApp = require('trails');
+const server = new TrailsApp(app);
 
-server.start().catch(err => server.stop(err))
+server
+  .start()
+  .catch(err => {
+    console.log(err);
+    console.trace();
+  });
