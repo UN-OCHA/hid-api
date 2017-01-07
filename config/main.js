76eb22ce462fb76afe30c82f32d40919c48f0a14
Guillaume Viguier <guillaume@viguierjust.com>
2017-01-07 15:57:23 +0100

    Reactivated trailpack-cron and trailpack-cache

--- config/main.js
+++ config/main.js
@@ -20,8 +20,8 @@ module.exports = {
     require('trailpack-router'),
     require('trailpack-hapi'),
     require('trailpack-mongoose'),
-    //require('trailpack-cron'),
-    //require('trailpack-cache')
+    require('trailpack-cron'),
+    require('trailpack-cache')
   ],
 
   /**
