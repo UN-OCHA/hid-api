53b51590a9816e25cd7298d36b855ec1eabce0ad
Guillaume Viguier <guillaume@viguierjust.com>
2017-01-17 11:14:25 +0100

    OPS-1743 - added newrelic configuration

--- server.js
+++ server.js
@@ -6,13 +6,13 @@
 
 'use strict';
 
-const app = require('./');
-const TrailsApp = require('trails');
-const server = new TrailsApp(app);
-
 if (process.env.NEW_RELIC_APP_NAME) {
   require('newrelic');
 }
+
+const app = require('./');
+const TrailsApp = require('trails');
+const server = new TrailsApp(app);
 
 server
   .start()
