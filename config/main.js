dde5798dac92c3216575df42e15f551951245149
Guillaume Viguier <guillaume@viguierjust.com>
2017-02-03 14:18:19 +0100

    Reactivated cron

--- config/main.js
+++ config/main.js
@@ -20,7 +20,7 @@ module.exports = {
     require('trailpack-router'),
     require('trailpack-hapi'),
     require('trailpack-mongoose'),
-    //require('trailpack-cron'),
+    require('trailpack-cron'),
     require('trailpack-cache')
   ],
 
