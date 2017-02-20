55c5f0d7ffffe93fbc7978c18aa822eac98a8de5
Guillaume Viguier <guillaume@viguierjust.com>
2017-02-20 12:05:33 +0100

    HID-1335 - delete duplicate accounts

--- api/controllers/DuplicateController.js
+++ api/controllers/DuplicateController.js
@@ -88,15 +88,19 @@ module.exports = class DuplicateController extends Controller{
   delete (request, reply) {
     const User = this.app.orm.User;
     const Duplicate = this.app.orm.Duplicate;
+    let that = this;
     User
       .remove({_id: request.params.id}, function (err) {
         Duplicate
-          .find({duplicates: request.params.id})
+          .findOne({duplicates: request.params.id})
           .then((dup) => {
             if (dup.duplicates.length === 2) {
               dup.remove();
             }
             reply();
+          })
+          .catch(err => {
+            that.app.services.ErrorService.handle(err, reply);
           });
       });
   }
