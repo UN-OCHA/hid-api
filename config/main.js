260d6c156f52005b912de99af5ac44d6cf6e8199
Guillaume Viguier <guillaume@viguierjust.com>
2017-03-28 16:10:33 +0200

    Fixed issue with footprints

--- config/main.js
+++ config/main.js
@@ -20,7 +20,6 @@ module.exports = {
     require('trailpack-router'),
     require('trailpack-hapi'),
     require('trailpack-mongoose'),
-    require('trailpack-footprints'),
     //require('trailpack-cron'),
     require('trailpack-cache')
   ],
