fbea7894f633cd618f49ddd7dcc75f19dbe36d55
Guillaume Viguier <guillaume@viguierjust.com>
2017-03-28 15:58:53 +0200

    Revert "Fixed issue with oauth clients"
    This reverts commit b8de0e10d4119de13f75476724864e8ecbc01317.

--- api/controllers/ClientController.js
+++ api/controllers/ClientController.js
@@ -15,26 +15,9 @@ module.exports = class ClientController extends Controller{
   }
 
   find (request, reply) {
-    const Client = this.app.orm.Client;
-    const options = this.app.services.HelperService.getOptionsFromQuery(request.query);
-    const criteria = this.app.services.HelperService.getCriteriaFromQuery(request.query);
-
-    const that = this;
-    const query = this.app.services.HelperService.find('Client', criteria, options);
-    query
-      .then((results) => {
-        return Client
-          .count(criteria)
-          .then((number) => {
-            return {result: results, number: number};
-          });
-      })
-      .then((result) => {
-        return reply(result.result).header('X-Total-Count', result.number);
-      })
-      .catch((err) => {
-        that.app.services.ErrorService.handle(err, reply);
-      });
+    request.params.model = 'client';
+    const FootprintController = this.app.controllers.FootprintController;
+    FootprintController.find(request, reply);
   }
 
   update (request, reply) {
