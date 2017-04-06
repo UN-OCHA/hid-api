0af58922daba9836a3b70df8499b17a1d8fdb291
Guillaume Viguier <guillaume@viguierjust.com>
2017-04-06 12:09:14 +0200

    Fixed client issues

--- api/controllers/ClientController.js
+++ api/controllers/ClientController.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Controller = require('trails/controller');
+const Boom = require('boom');
 
 /**
  * @module ClientController
@@ -19,22 +20,39 @@ module.exports = class ClientController extends Controller{
     const options = this.app.services.HelperService.getOptionsFromQuery(request.query);
     const criteria = this.app.services.HelperService.getCriteriaFromQuery(request.query);
 
-    const that = this;
-    const query = this.app.services.HelperService.find('Client', criteria, options);
-    query
-      .then((results) => {
-        return Client
-          .count(criteria)
-          .then((number) => {
-            return {result: results, number: number};
-          });
-      })
-      .then((result) => {
-        return reply(result.result).header('X-Total-Count', result.number);
-      })
-      .catch((err) => {
-        that.app.services.ErrorService.handle(err, reply);
-      });
+    if (request.params.id) {
+      const that = this;
+      criteria._id = request.params.id;
+      Client
+        .findOne(criteria)
+        .populate(options.populate)
+        .then(result => {
+          if (!result) {
+            throw Boom.notFound();
+          }
+
+          result.sanitize(request.params.currentUser);
+          return reply(result);
+        })
+        .catch(err => { that.app.services.ErrorService.handle(err, reply); });
+    }
+    else {
+      const query = this.app.services.HelperService.find('Client', criteria, options);
+      query
+        .then((results) => {
+          return Client
+            .count(criteria)
+            .then((number) => {
+              return {result: results, number: number};
+            });
+        })
+        .then((result) => {
+          return reply(result.result).header('X-Total-Count', result.number);
+        })
+        .catch((err) => {
+          that.app.services.ErrorService.handle(err, reply);
+        });
+    }
   }
 
   update (request, reply) {
