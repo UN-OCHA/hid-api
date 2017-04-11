85ee61229fcaf28f47921ff908b20bc1634af178
Guillaume Viguier <guillaume@viguierjust.com>
2017-04-11 11:25:11 +0200

    Kaya fixes

--- api/services/JwtService.js
+++ api/services/JwtService.js
@@ -41,7 +41,7 @@ module.exports = class JwtService extends Service {
   generateIdToken (client, user, nonce) {
     const now = Math.floor(Date.now() / 1000);
     let sub = user._id;
-    if (client.id === 'iasc-prod') {
+    if (client.id === 'iasc-prod' || client.id === 'kaya-dev') {
       sub = user.email;
     }
     const idToken = {
