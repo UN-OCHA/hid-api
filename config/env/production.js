731ebf37dbc45b7cfb432c4e0cccfb7de95abc20
Guillaume Viguier <guillaume@viguierjust.com>
2017-08-07 14:32:13 +0200

    Fixed logging for production

--- config/env/production.js
+++ config/env/production.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const winston = require('winston');
+const os = require('os');
+const _ = require('lodash');
 
 module.exports = {
 
@@ -35,6 +37,22 @@ module.exports = {
     logger: new winston.Logger({
       level: 'info',
       exitOnError: false,
+      rewriters: [
+        function (level, msg, metadata) {
+          delete metadata.request;
+
+          // Keep original metadata safe.
+          metadata = _.clone(metadata || {});
+
+          // Extend metadata with some default.
+          metadata.level = level;
+          metadata.hostname = os.hostname();
+          metadata.env = 'hid-' + process.env.NODE_ENV;
+          metadata['@timestamp'] = new Date().toJSON();
+
+          return metadata;
+        }
+      ],
       transports: [
         new winston.transports.Console({
           timestamp: true
