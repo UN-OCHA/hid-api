b51cab6293a05daf33c203c27f693a1744aa803d
Guillaume Viguier <guillaume@viguierjust.com>
2017-08-17 16:09:29 +0200

    HID-1523 - rate limit

--- config/web.js
+++ config/web.js
@@ -59,7 +59,7 @@ module.exports = {
     {
       register: require('hapi-rate-limit'),
       options: {
-        userLimit: 3000,
+        userLimit: 1,
         trustProxy: true,
         pathLimit: false
       }
