9597d3c4e44d43ab9fb5b5344fa15e6bffaaf04a
Guillaume Viguier <guillaume@viguierjust.com>
2017-12-07 11:03:39 +0100

    OAuth token expiration

--- api/models/OauthToken.js
+++ api/models/OauthToken.js
@@ -42,6 +42,8 @@ module.exports = class OauthToken extends Model {
         isExpired: function () {
           const now = new Date();
           const expires = this.expires;
+          console.log(expires.getTime());
+          console.log(now.getTime());
           return now.getTime() < expires.getTime();
         }
       }
