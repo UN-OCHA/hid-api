e811459e9d7dde86796338b1ab09272e21ca0071
Guillaume Viguier <guillaume@viguierjust.com>
2017-12-14 16:03:04 +0100

    HID-1579 - read spreadsheet values

--- api/controllers/GSSSyncController.js
+++ api/controllers/GSSSyncController.js
@@ -2,6 +2,8 @@
 
 const Controller = require('trails/controller');
 const Boom = require('boom');
+const Google = require('googleapis');
+const GoogleAuth = require('google-auth-library');
 
 /**
  * @module GSSSyncController
@@ -12,7 +14,19 @@ module.exports = class GSSSyncController extends Controller{
   create (request, reply) {
     const GSSSync = this.app.orm.GSSSync;
     const that = this;
-    GSSSync
+    const authFactory = new GoogleAuth();
+    request.payload.user = request.params.currentUser._id;
+    if (request.payload.access_token) {
+      authFactory.getApplicationDefault(function (err, authClient) {
+        authClient.credentials = request.payload.access_token;
+        that._readSpreadsheet(authClient, request.payload.spreadsheet, reply);
+      });
+
+    }
+    else {
+      reply();
+    }
+    /*GSSSync
       .create(request.payload)
       .then((gsssync) => {
         if (!gsssync) {
@@ -22,7 +36,22 @@ module.exports = class GSSSyncController extends Controller{
       })
       .catch(err => {
         that.app.services.ErrorService.handle(err, request, reply);
-      });
+      });*/
+  }
+
+  _readSpreadsheet (auth, spreadsheetId, reply) {
+    const sheets = Google.sheets('v4');
+    sheets.spreadsheets.values.get({
+      auth: auth,
+      spreadsheetId: spreadsheetId,
+      range: 'A1:D1',
+    }, function(err, response) {
+      if (err) {
+        console.log('The API returned an error: ' + err);
+        return;
+      }
+      reply(response.values);
+    });
   }
 
   destroy (request, reply) {
