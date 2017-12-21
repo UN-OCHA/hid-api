c1330dccf606f21c9af8b048b20412ec3c7d2fd5
Guillaume Viguier <guillaume@viguierjust.com>
2017-12-21 14:03:50 +0100

    HID-1586 - move cron tasks to croncontroller

--- api/controllers/CronController.js
+++ api/controllers/CronController.js
@@ -2,6 +2,8 @@
 
 const Controller = require('trails/controller');
 const async = require('async');
+const _ = require('lodash');
+const https = require('https');
 const listAttributes = [
   'lists',
   'operations',
@@ -51,9 +53,295 @@ module.exports = class CronController extends Controller{
     });
   }
 
+  // Notify users of a new disaster
+  _notifyNewDisaster (disaster) {
+    const app = this.app;
+    const List = app.orm.list;
+    const User = app.orm.User;
+    const NotificationService = app.services.NotificationService;
+    if (disaster.metadata.operation && disaster.metadata.operation.length) {
+      let operation = {};
+      for (let i = 0, len = disaster.metadata.operation.length; i < len; i++) {
+        operation = disaster.metadata.operation[i];
+        List
+          .findOne({remote_id: operation.id})
+          .then((list) => {
+            if (!list) {
+              throw new Error('List not found');
+            }
+            return User
+              .find({operations: { $elemMatch: { list: list._id, deleted: false }} });
+          })
+          .then((users) => {
+            const notification = {type: 'new_disaster', params: {list: disaster}};
+            app.log.debug('Notifying ' + users.length + ' users of a new disaster: ' + disaster.label);
+            NotificationService.sendMultiple(users, notification, () => { });
+          })
+          .catch((err) => {});
+      }
+    }
+  }
+
+  _createListHelper (list, cb) {
+    const List = this.app.orm.List;
+    const app = this.app;
+    const that = this;
+    List.create(list, function (err, li) {
+      if (err) {
+        app.log.error(err);
+        return cb(err);
+      }
+      if (li.type === 'disaster') {
+        that._notifyNewDisaster(li);
+      }
+      cb();
+    });
+  }
+
+  // Create a list based on the item pulled from hrinfo
+  _createList (listType, language, item, cb) {
+    const app = this.app;
+    const List = this.app.orm.List;
+    const User = this.app.orm.User;
+    const that = this;
+    const inactiveOps = [2782,2785,2791,38230];
+    if ((listType === 'operation' &&
+      item.status !== 'inactive' &&
+      inactiveOps.indexOf(item.id) === -1 &&
+      item.hid_access !== 'inactive') ||
+      listType !== 'operation') {
+      List.findOne({type: listType, remote_id: item.id}, function (err, list) {
+        if (!list) {
+          that._parseList(listType, language, item, function (newList) {
+            that._parseListLanguage(newList, newList.label, newList.acronym, language);
+            that._createListHelper(newList, cb);
+          });
+        }
+        else {
+          that._parseList(listType, language, item, function (newList) {
+            let updateUsers = false;
+            if (newList.name !== list.name || newList.visibility !== list.visibility) {
+              updateUsers = true;
+            }
+            // Do not change list visibility or joinability if the list is already there
+            delete newList.visibility;
+            delete newList.joinability;
+            that._parseListLanguage(list, newList.label, newList.acronym, language);
+            if (language !== 'en') {
+              delete newList.label;
+              delete newList.acronym;
+            }
+            _.merge(list, newList);
+            list.save().then(function (list) {
+              if (updateUsers) {
+                const criteria = {};
+                criteria[list.type + 's.list'] = list._id.toString();
+                User
+                  .find(criteria)
+                  .then(users => {
+                    let user = {};
+                    for (let i = 0; i < users.length; i++) {
+                      user = users[i];
+                      for (let j = 0; j < user[list.type + 's'].length; j++) {
+                        if (user[list.type + 's'][j].list === list._id) {
+                          user[list.type + 's'][j].name = list.name;
+                          user[list.type + 's'][j].names = list.names;
+                          user[list.type + 's'][j].acronym = list.acronym;
+                          user[list.type + 's'][j].acronyms = list.acronyms;
+                          user[list.type + 's'][j].visibility = list.visibility;
+                        }
+                      }
+                      user.save();
+                    }
+                    cb();
+                  }
+                );
+              }
+              else {
+                cb();
+              }
+            });
+          });
+        }
+      });
+    }
+    else {
+      cb();
+    }
+  }
+
+  _parseList (listType, language, item, cb) {
+    const List = this.app.orm.List;
+    let visibility = '', label = '', acronym = '', tmpList = {};
+    visibility = 'all';
+    if (item.hid_access && item.hid_access === 'closed') {
+      visibility = 'verified';
+    }
+    label = item.label;
+    if (listType === 'bundle' || listType === 'office') {
+      if (item.operation[0].label) {
+        label = item.operation[0].label + ': ' + item.label;
+      }
+      else {
+        label = 'Global: ' + item.label;
+      }
+    }
+    if (listType === 'organization' && item.acronym) {
+      acronym = item.acronym;
+    }
+    tmpList = {
+      label: label,
+      acronym: acronym,
+      type: listType,
+      visibility: visibility,
+      joinability: 'public',
+      remote_id: item.id,
+      metadata: item
+    };
+
+    if (listType === 'bundle') {
+      List
+        .findOne({type: 'operation', remote_id: item.operation[0].id})
+        .then((op) => {
+          if (op) {
+            if (op.metadata.hid_access) {
+              if (op.metadata.hid_access === 'open') {
+                tmpList.visibility = 'all';
+              }
+              else if (op.metadata.hid_access === 'closed') {
+                tmpList.visibility = 'verified';
+              }
+            }
+          }
+          cb(tmpList);
+        });
+    }
+    else {
+      cb(tmpList);
+    }
+  }
+
+  _parseListLanguage (list, label, acronym, language) {
+    let labelFound = false;
+    if (list.labels && list.labels.length) {
+      for (let i = 0; i < list.labels.length; i++) {
+        if (list.labels[i].language === language) {
+          labelFound = true;
+          list.labels[i].text = label;
+        }
+      }
+    }
+    else {
+      list.labels = [];
+    }
+    if (!labelFound) {
+      list.labels.push({language: language, text: label});
+    }
+
+    let acronymFound = false;
+    if (list.acronyms && list.acronyms.length) {
+      for (let j = 0; j < list.acronyms.length; j++) {
+        if (list.acronyms[j].language === language) {
+          acronymFound = true;
+          list.acronyms[j].text = acronym;
+        }
+      }
+    }
+    else {
+      list.acronyms = [];
+    }
+    if (!acronymFound) {
+      list.acronyms.push({language: language, text: acronym});
+    }
+  }
+
   importLists (request, reply) {
+    const app = this.app;
+    const List = app.orm.list;
+    const User = app.orm.user;
+    const NotificationService = app.services.NotificationService;
+    const now = Math.floor(Date.now() / 1000);
+    const languages = ['en', 'fr', 'es'];
+    const that = this;
+    //const Cache = app.services.CacheService.getCaches(['local-cache'])
+    let hasNextPage = false, pageNumber = 1, path = '';
+
     reply().code(204);
-    this.app.config.cron.importLists(this.app);
+
+    let lastPull = Math.round(Date.now() / 1000) - 7 * 24 * 3600;
+    //Cache.then((mongoCache) => {
+      //return mongoCache.get('lastPull', function (err, lastPull) {
+        //if (err) app.log.info(err)
+    if (!lastPull) {
+      lastPull = 0;
+    }
+    async.eachSeries(languages, function (language, nextLanguage) {
+      // For each list type
+      async.eachSeries(listTypes,
+        function(listType, nextType) {
+          // Parse while there are pages
+          async.doWhilst(function (nextPage) {
+            path = '/' + language + '/api/v1.0/' + listType + 's?page=' + pageNumber + '&filter[created][value]=' + lastPull + '&filter[created][operator]=>';
+            if (listType === 'organization' || listType === 'functional_role') {
+              path = '/' + language + '/api/v1.0/' + listType + 's?page=' + pageNumber;
+            }
+            https.get({
+              host: 'www.humanitarianresponse.info',
+              port: 443,
+              path: path
+            }, function (response) {
+              pageNumber++;
+              let body = '';
+              response.on('data', function (d) {
+                body += d;
+              });
+              response.on('end', function() {
+                let parsed = {};
+                try {
+                  parsed = JSON.parse(body);
+                  hasNextPage = parsed.next ? true : false;
+                  async.eachSeries(parsed.data, function (item, cb) {
+                    // Do not add disasters more than 2 years old
+                    if (listType !== 'disaster' || (listType === 'disaster' && now - item.created < 2 * 365 * 24 * 3600)) {
+                      that._createList(listType, language, item, cb);
+                    }
+                    else {
+                      cb();
+                    }
+                  }, function (err) {
+                    setTimeout(function() {
+                      app.log.info('Done loading page ' + pageNumber + ' for ' + listType);
+                      nextPage();
+                    }, 1000);
+                  });
+                }
+                catch (e) {
+                  app.log.error('Error parsing hrinfo API.', { error: e });
+                }
+              });
+            });
+          }, function () {
+            return hasNextPage;
+          }, function (err, results) {
+            pageNumber = 1;
+            app.log.info('Done processing all ' + listType + 's');
+            nextType();
+          });
+        }, function (err) {
+          const currentTime = Math.round(Date.now() / 1000);
+          // Keep item in cache 12 minutes (720 seconds)
+          /*mongoCache.set('lastPull', currentTime, {ttl: 720}, function (err) {
+            app.log.info(err);
+          });*/
+          app.log.info('Done processing all list types for ' + language + ' at ' + currentTime);
+          nextLanguage();
+        }
+      );
+    }, function (err) {
+      app.log.info('Done importing lists');
+    });
+      //});
+    //});
   }
 
   deleteExpiredUsers (request, reply) {
@@ -362,5 +650,7 @@ module.exports = class CronController extends Controller{
       reply().code(204);
     });
   }
+
+
 
 };
