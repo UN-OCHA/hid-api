4ab29c47a839266e7ed55602c5d883f9c0bf47dd
Guillaume Viguier <guillaume@viguierjust.com>
2018-03-07 16:26:32 +0100

    Configure staging environment

--- config/env/staging.js
+++ config/env/staging.js
@@ -1,3 +1,25 @@
 'use strict';
 
-module.exports = {};
+module.exports = {
+  database: {
+    stores: {
+      staging: {
+        // should be 'create' or 'drop'
+        migrate: 'create',
+        uri: 'mongodb://db:27017/staging',
+        options: {
+          server: {
+            socketOptions: {
+              keepAlive: 600000,
+              connectTimeoutMS: 60000
+            }
+          }
+        }
+      }
+    },
+    models: {
+      defaultStore: 'staging',
+      migrate: 'create'
+    }
+  }
+};
