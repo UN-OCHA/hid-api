04115b687dd00cfb32d70c7abcc836a5dc48c59d
Guillaume Viguier <guillaume@viguierjust.com>
2019-01-15 10:42:02 +0100

    HID-1735 - remove setHidden function

--- config/routes.js
+++ config/routes.js
@@ -629,12 +629,6 @@ module.exports = [
   },
 
   {
-    method: 'GET',
-    path: '/api/v2/cron/setHidden',
-    handler: 'CronController.setHidden'
-  },
-
-  {
     method: 'POST',
     path: '/api/v2/webhooks/hrinfo',
     handler: 'WebhooksController.hrinfo'
