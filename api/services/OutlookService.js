c3a06dc8870422c64c0f2215c09efb748303f211
Guillaume Viguier <guillaume@viguierjust.com>
2019-02-13 14:48:46 +0100

    HID-1738 - hapi upgrade

--- api/services/OutlookService.js
+++ api/services/OutlookService.js
@@ -13,54 +13,42 @@ module.exports = {
       .find({list: listId});
   },
 
-  addUserToContactFolders: function (listId, user) {
-    return this
-      .findByList(listId)
-      .then(osyncs => {
-        if (osyncs.length) {
-          const fn = function (osync) {
-            return osync.addUser(user);
-          };
-          const actions = osyncs.map(fn);
-          return Promise.all(actions);
-        }
-      });
+  addUserToContactFolders: async function (listId, user) {
+    const osyncs = await OutlookSync.find({list: listId});
+    if (osyncs.length) {
+      const fn = function (osync) {
+        return osync.addUser(user);
+      };
+      const actions = osyncs.map(fn);
+      return Promise.all(actions);
+    }
   },
 
-  deleteUserFromContactFolders: function (listId, userId) {
-    return this
-      .findByList(listId)
-      .then(osyncs => {
-        if (osyncs.length) {
-          const fn = function (osync) {
-            return osync.deleteUser(userId);
-          };
-          const actions = osyncs.map(fn);
-          return Promise.all(actions);
-        }
-      });
+  deleteUserFromContactFolders: async function (listId, userId) {
+    const osyncs = await OutlookSync.find({list: listId});
+    if (osyncs.length) {
+      const fn = function (osync) {
+        return osync.deleteUser(userId);
+      };
+      const actions = osyncs.map(fn);
+      return Promise.all(actions);
+    }
   },
 
-  synchronizeUser: function (user) {
+  synchronizeUser: async function (user) {
     // Get all lists from user
     const listIds = user.getListIds();
 
     // Find the gsssyncs associated to the lists
-    return OutlookSync
-      .find({list: {$in: listIds}})
-      .then(osyncs => {
-        if (osyncs.length) {
-          // For each gsssync, call updateUser
-          const fn = function (osync) {
-            return osync.updateUser(user);
-          };
-          const actions = osyncs.map(fn);
-          return Promise.all(actions);
-        }
-      })
-      .then(values => {
-        return user;
-      });
+    const osyncs = await OutlookSync.find({list: {$in: listIds}});
+    if (osyncs.length) {
+      // For each gsssync, call updateUser
+      const fn = function (osync) {
+        return osync.updateUser(user);
+      };
+      const actions = osyncs.map(fn);
+      return Promise.all(actions);
+    }
   }
 
 };
