04ee461667a051dc9b5d9a8573029fe595d24d84
Peter Droogmans (attiks) <peter@attiks.com>
2020-01-27 15:11:48 +0100

    Comments

--- commands/createDummyUser.js
+++ commands/createDummyUser.js
@@ -1,8 +1,10 @@
 /* eslint no-await-in-loop: "off", no-restricted-syntax: "off", no-console: "off" */
 /* eslint func-names: "off" */
 /**
- * @module deleteExpiredUsers
- * @description Deletes the expired users from the database.
+ * @module createDummyUser
+ * @description Create a test user.
+ *
+ * docker-compose exec dev node ./commands/createDummyUser.js
  */
 const mongoose = require('mongoose');
 const app = require('../');
@@ -10,20 +12,16 @@ const app = require('../');
 const store = app.config.env[process.env.NODE_ENV].database.stores[process.env.NODE_ENV];
 mongoose.connect(store.uri, store.options);
 
-const hidAccount = '5b2128e754a0d6046d6c69f2';
 const User = require('../api/models/User');
-const EmailService = require('../api/services/EmailService');
 
 async function run() {
-  const now = new Date();
-  const start = new Date(2016, 0, 1, 0, 0, 0);
+  let reginfo = {
+    email: 'test@example.com',
+    password: 'testing',
+    family_name: 'test',
+    given_name: 'test'
+  };
 
-  let reginfo = {};
-
-  reginfo.email = 'test@example.com';
-  reginfo.password = 'testing';
-  reginfo.family_name = 'test';
-  reginfo.given_name = 'test';
   reginfo.confirm_password = reginfo.password;
 
   reginfo.emails = [];
