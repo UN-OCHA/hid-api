1cea7c9b1b6b69ba404eaf3ece039055d719c558
Chris Ruppel <ruppel@reliefweb.int>
2021-01-14 09:36:39 +0100

    HID-2009: move the confirmation JS and make it generic

--- assets/js/profile-edit.js
+++
@@ -1,6 +0,0 @@
-/**
- * Helper function to confirm an email deletion.
- */
-function confirmDeletion (emailAddress) {
-  return window.confirm('Are you sure you want to remove' + emailAddress + ' from your profile?');
-}
