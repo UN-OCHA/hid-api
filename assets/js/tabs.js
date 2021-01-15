9f831c6627dfa7b2cb8db820ceb3f4d5e34b3f88
Chris Ruppel <ruppel@reliefweb.int>
2021-01-15 11:06:12 +0100

    HID-2158: copy settings template and reorganize/rename some CSS

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -7,21 +7,21 @@
   const panels = tabbed.querySelectorAll('[id^="section"]');
 
   // The tab switching function
-  const switchTab = (oldTab, newTab) => {
-    newTab.focus();
-    // Make the active tab focusable by the user (Tab key)
-    newTab.removeAttribute('tabindex');
-    // Set the selected state
-    newTab.setAttribute('aria-selected', 'true');
-    oldTab.removeAttribute('aria-selected');
-    oldTab.setAttribute('tabindex', '-1');
-    // Get the indices of the new and old tabs to find the correct
-    // tab panels to show and hide
-    let index = Array.prototype.indexOf.call(tabs, newTab);
-    let oldIndex = Array.prototype.indexOf.call(tabs, oldTab);
-    panels[oldIndex].hidden = true;
-    panels[index].hidden = false;
-  };
+  // const switchTab = (oldTab, newTab) => {
+  //   newTab.focus();
+  //   // Make the active tab focusable by the user (Tab key)
+  //   newTab.removeAttribute('tabindex');
+  //   // Set the selected state
+  //   newTab.setAttribute('aria-selected', 'true');
+  //   oldTab.removeAttribute('aria-selected');
+  //   oldTab.setAttribute('tabindex', '-1');
+  //   // Get the indices of the new and old tabs to find the correct
+  //   // tab panels to show and hide
+  //   let index = Array.prototype.indexOf.call(tabs, newTab);
+  //   let oldIndex = Array.prototype.indexOf.call(tabs, oldTab);
+  //   panels[oldIndex].hidden = true;
+  //   panels[index].hidden = false;
+  // };
 
   // Add the tablist role to the first <ul> in the .tabbed container
   tablist.setAttribute('role', 'tablist');
@@ -34,28 +34,28 @@
     tab.parentNode.setAttribute('role', 'presentation');
 
     // Handle clicking of tabs for mouse users
-    tab.addEventListener('click', e => {
-      e.preventDefault();
-      let currentTab = tablist.querySelector('[aria-selected]');
-      if (e.currentTarget !== currentTab) {
-        switchTab(currentTab, e.currentTarget);
-      }
-    });
+    // tab.addEventListener('click', e => {
+    //   e.preventDefault();
+    //   let currentTab = tablist.querySelector('[aria-selected]');
+    //   if (e.currentTarget !== currentTab) {
+    //     switchTab(currentTab, e.currentTarget);
+    //   }
+    // });
 
     // Handle keydown events for keyboard users
-    tab.addEventListener('keydown', e => {
-      // Get the index of the current tab in the tabs node list
-      let index = Array.prototype.indexOf.call(tabs, e.currentTarget);
-      // Work out which key the user is pressing and
-      // Calculate the new tab's index where appropriate
-      let dir = e.which === 37 ? index - 1 : e.which === 39 ? index + 1 : e.which === 40 ? 'down' : null;
-      if (dir !== null) {
-        e.preventDefault();
-        // If the down key is pressed, move focus to the open panel,
-        // otherwise switch to the adjacent tab
-        dir === 'down' ? panels[i].focus() : tabs[dir] ? switchTab(e.currentTarget, tabs[dir]) : void 0;
-      }
-    });
+    // tab.addEventListener('keydown', e => {
+    //   // Get the index of the current tab in the tabs node list
+    //   let index = Array.prototype.indexOf.call(tabs, e.currentTarget);
+    //   // Work out which key the user is pressing and
+    //   // Calculate the new tab's index where appropriate
+    //   let dir = e.which === 37 ? index - 1 : e.which === 39 ? index + 1 : e.which === 40 ? 'down' : null;
+    //   if (dir !== null) {
+    //     e.preventDefault();
+    //     // If the down key is pressed, move focus to the open panel,
+    //     // otherwise switch to the adjacent tab
+    //     dir === 'down' ? panels[i].focus() : tabs[dir] ? switchTab(e.currentTarget, tabs[dir]) : void 0;
+    //   }
+    // });
   });
 
   // Add tab panel semantics and hide them all
