43cf1e5049fec7713a8e5c9bc59120ef7ca1383b
Chris Ruppel <ruppel@reliefweb.int>
2021-02-18 14:32:00 +0100

    HID-2191: attempt to fix env var processing to restore mailhog on dev/stage

--- api/services/EmailService.js
+++ api/services/EmailService.js
@@ -4,7 +4,7 @@ const Email = require('email-templates');
 const TransporterSettings = {
   host: process.env.SMTP_HOST || 'localhost',
   port: process.env.SMTP_PORT || 25,
-  secure: process.env.SMTP_TLS || false,
+  secure: typeof process.env.SMTP_TLS !== 'undefined' ? JSON.parse(process.env.SMTP_TLS) : false,
 };
 // Only append `auth` property if we have both values to pass.
 if (process.env.SMTP_USER && process.env.SMTP_PASS) {
