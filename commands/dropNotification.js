58cbf2130c3e56b7e9ef205a3ad6910b15e7af8d
Chris Ruppel <ruppel@reliefweb.int>
2021-02-19 14:56:40 +0100

    HID-1972: migration to drop Notification collection

---
+++ commands/dropNotification.js
@@ -0,0 +1,45 @@
+/**
+ * @module dropNotifications
+ * @description Drops the Notifications collection permanently.
+ *
+ * docker-compose exec dev node ./commands/dropNotifications.js
+ */
+const mongoose = require('mongoose');
+const app = require('../');
+
+const logger = app.config.env[process.env.NODE_ENV].logger;
+const store = app.config.env[process.env.NODE_ENV].database.stores[process.env.NODE_ENV];
+mongoose.connect(store.uri, store.options);
+
+const Notification = require('../api/models/Notification');
+
+async function run() {
+  // Drop the Notifications collection
+  await Notification.collection.drop().then(data => {
+    logger.info(
+      '[commands->dropNotification] dropped Notification collection',
+    );
+  }).catch(err => {
+    logger.warn(
+      '[commands->dropNotification] failed to drop Notification collection',
+      {
+        fail: true,
+        stack_trace: err.stack,
+      },
+    );
+  });
+
+  process.exit();
+}
+
+(async function () {
+  await run();
+}()).catch(err => {
+  logger.error(
+    `[commands->dropNotification] ${err.message}`,
+    {
+      fail: true,
+    },
+  );
+  process.exit(1);
+});
