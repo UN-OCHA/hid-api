31e5fe4f314383dd9d1bbd7ac15951fd29f5eb3d
Chris Ruppel <ruppel@reliefweb.int>
2021-03-26 11:13:03 +0100

    HID-2213: E2E for password reset, step 2

--- _tests/e2e/PasswordReset.test.js
+++ _tests/e2e/PasswordReset.test.js
@@ -35,9 +35,25 @@ describe('PasswordReset', () => {
   it('shows positive feedback after submitting valid email address', async () => {
     const input = await page.$('#email');
     await input.click({ clickCount: 3 });
-    await page.type('#email', 'e2e-test@example.com');
+    await page.type('#email', env.testUserEmail);
     await page.click('.t-btn--reset');
     await page.waitForTimeout(1000);
     expect(await page.content()).toContain('The request to change your password has been received.');
+  });
+
+  it('allows user to initiate password reset via email', async () => {
+    // Mailhog is here when you set up via hid-stack
+    await page.goto('http://localhost:8025');
+    // We always want the first message in Mailhog.
+    await page.click('.messages > *:first-child');
+    // Target this message's iframe in Mailhog.
+    const message = await page.frames()[1];
+    // Mailhog has iframes and the link has target="_blank" and all of that makes
+    // it a real PITA to truly click the link and follow it. Let's instead grab
+    // the URL and go directly to it:
+    const pwResetUrl = await message.$eval('p:nth-child(3) a', el => el.innerText);
+    await page.goto(pwResetUrl);
+    // Do we see the password reset form?
+    expect(await page.content()).toContain('Enter your new password');
   });
 });
