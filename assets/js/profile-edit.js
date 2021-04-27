4f8bc1119bb20e188510e712c4bcce40bea6e4f8
Chris Ruppel <ruppel@reliefweb.int>
2021-04-27 14:54:49 +0200

    HID-2218: ensure other actions can take place after declining a prompt

--- assets/js/profile-edit.js
+++ assets/js/profile-edit.js
@@ -5,14 +5,14 @@
  * - confirmation.js
  */
 (function iife () {
-  var formEmails = document.querySelector('.form--profile-emails');
-  var formActions = document.querySelectorAll('button[name="email_delete"]');
+  var form = document.querySelector('.form--profile-emails');
+  var formActions = document.querySelectorAll('.form--profile-emails button');
   var formAction = null;
   var emailToDelete = null;
 
   // Require a confirmation before deleting an email from the profile.
   // Any other submission should drop through.
-  formEmails.addEventListener('submit', function (ev) {
+  form.addEventListener('submit', function (ev) {
     // If the user is removing an email, confirm it first.
     // Otherwise allow the form to submit like normal.
     if (formAction !== 'email_delete' || confirmRemoval(emailToDelete)) {
@@ -27,7 +27,7 @@
   // confirmation of the deletion.
   formActions.forEach(function (btn) {
     btn.addEventListener('click', function (ev) {
-      formAction = 'email_delete';
+      formAction = btn.name;
       emailToDelete = btn.value;
     });
   });
