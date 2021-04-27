6cc9f190e33f2a02b63f9cdf773719bfc9bc420c
Chris Ruppel <ruppel@reliefweb.int>
2021-04-27 14:38:20 +0200

    HID-2218: far more compatible solution for prompting removals

--- assets/js/profile-edit.js
+++ assets/js/profile-edit.js
@@ -6,18 +6,29 @@
  */
 (function iife () {
   var formEmails = document.querySelector('.form--profile-emails');
+  var formActions = document.querySelectorAll('button[name="email_delete"]');
+  var formAction = null;
+  var emailToDelete = null;
 
   // Require a confirmation before deleting an email from the profile.
+  // Any other submission should drop through.
   formEmails.addEventListener('submit', function (ev) {
-    var formAction = ev.submitter.name;
-    var emailToDelete = ev.submitter.value;
-
     // If the user is removing an email, confirm it first.
     // Otherwise allow the form to submit like normal.
     if (formAction !== 'email_delete' || confirmRemoval(emailToDelete)) {
-      // Allow the submission
+      // Allow the submission.
     } else {
+      // Prevent the submission.
       ev.preventDefault();
     }
+  });
+
+  // When triggering the delete buttons, set up the submit handler to require
+  // confirmation of the deletion.
+  formActions.forEach(function (btn) {
+    btn.addEventListener('click', function (ev) {
+      formAction = 'email_delete';
+      emailToDelete = btn.value;
+    });
   });
 }());
