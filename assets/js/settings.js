829b71f20ac400efa73570a2abc056c4968335cf
Chris Ruppel <ruppel@reliefweb.int>
2021-04-29 15:27:48 +0200

    HID-1828: lint settings.js

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,17 +1,19 @@
+/* global confirmRemoval */
+
 /**
  * Settings
  *
  * Dependencies:
  * - confirmation.js
  */
-(function iife () {
+(function iife() {
   var form = document.querySelector('.form--settings-oauth-clients');
   var formActions = form.querySelectorAll('button');
   var formAction = null;
   var clientToRevoke = null;
 
   // Validate form submissions.
-  form.addEventListener('submit', function (ev) {
+  form.addEventListener('submit', function settingsSubmitHandler(ev) {
     // The form allows most submissions by default.
     // However if the user is revoking an OAuth Client, confirm it first.
     if (formAction !== 'oauth_client_revoke' || confirmRemoval(clientToRevoke)) {
@@ -24,8 +26,8 @@
 
   // When triggering the delete buttons, set up the submit handler to require
   // confirmation of the deletion.
-  formActions.forEach(function (btn) {
-    btn.addEventListener('click', function (ev) {
+  formActions.forEach(function settingsAssignButtonHandler(btn) {
+    btn.addEventListener('click', function settingsButtonHandler() {
       formAction = btn.name;
       clientToRevoke = btn.dataset.clientName;
     });
