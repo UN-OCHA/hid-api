72286c9bc454bb0936fbb0bb5f8fa7543a6620f8
Chris Ruppel <ruppel@reliefweb.int>
2021-06-17 09:24:28 +0200

    HID-2230: thx @cafuego

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -427,7 +427,7 @@ module.exports = {
 
       let alertMessage = 'We could not log you in. The username or password you have entered are incorrect. Kindly try again.';
       if (err.message === 'password is expired') {
-        alertMessage = 'We could not log you in because your password is expired. Following UN regulations, as a security measure passwords must be udpated every six months. Kindly reset your password by clicking on the "Forgot/Reset password" link below.';
+        alertMessage = 'We could not log you in because your password is expired. Following UN regulations, as a security measure passwords must be updated every six months. Kindly reset your password by clicking on the "Forgot/Reset password" link below.';
       }
 
       // Display login form to user.
