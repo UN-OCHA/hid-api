df4b0ed1c79ee76229dd21a05242aa8c7ca3b1b3
Chris Ruppel <ruppel@reliefweb.int>
2021-08-10 15:36:26 +0200

    HID-2248: whitespace

--- api/controllers/ViewController.js
+++ api/controllers/ViewController.js
@@ -361,7 +361,10 @@ module.exports = {
   async newPassword(request, reply) {
     request.yar.reset();
     request.yar.set('session', {
-      hash: request.query.hash, id: request.query.id, time: request.query.time, totp: false,
+      hash: request.query.hash,
+      id: request.query.id,
+      time: request.query.time,
+      totp: false,
     });
 
     const user = await User.findOne({ _id: request.query.id });
@@ -379,7 +382,10 @@ module.exports = {
     }
 
     request.yar.set('session', {
-      hash: request.query.hash, id: request.query.id, time: request.query.time, totp: true,
+      hash: request.query.hash,
+      id: request.query.id,
+      time: request.query.time,
+      totp: true,
     });
 
     return reply.view('new_password', {
@@ -397,9 +403,11 @@ module.exports = {
       try {
         const user = await User.findOne({ _id: cookie.id });
         const token = request.payload['x-hid-totp'];
+
         await AuthPolicy.isTOTPValid(user, token);
         cookie.totp = true;
         request.yar.set('session', cookie);
+
         return reply.view('new_password', {
           query: request.payload,
           hash: cookie.hash,
@@ -423,8 +431,10 @@ module.exports = {
       const params = HelperService.getOauthParams(request.payload);
       const registerLink = _getRegisterLink(request.payload);
       const passwordLink = _getPasswordLink(request.payload);
+
       try {
         await UserController.resetPasswordEndpoint(request);
+
         if (params) {
           return reply.view('login', {
             alert: {
@@ -436,6 +446,7 @@ module.exports = {
             passwordLink,
           });
         }
+
         return reply.view('message', {
           alert: {
             type: 'status',
@@ -471,7 +482,10 @@ module.exports = {
     }
 
     return reply.view('message', {
-      alert: { type: 'error', message: 'There was an error resetting your password.' },
+      alert: {
+        type: 'error',
+        message: 'There was an error resetting your password.',
+      },
       query: request.payload,
       isSuccess: false,
       title: 'Password update',
