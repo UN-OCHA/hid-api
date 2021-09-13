UN-OCHA/hid-api

commit 8b950252568dbdec1988b9c3d00b119c06591a88

HID-2260: more logs (and comments) to debug load balancer problem

api/controllers/ViewController.js
@@ -125,7 +125,13 @@ module.exports = {
   },
 
   async logout(request, reply) {
+    // Temporarily store user session for logging purposes.
+    const cookie = request.yar.get('session');
+
+    // Destroy user session.
     request.yar.reset();
+
+    // Determine whether to redirect the user elsewhere besides HID homepage.
     if (request.query.redirect) {
       // Validate redirect URL
       const url = request.query.redirect;
@@ -144,13 +150,23 @@ module.exports = {
       try {
         const count = await Client.countDocuments({ redirectUri: regex });
         if (count > 0) {
+          logger.info(
+            `[ViewController->logout] Logging user out and redirecting to destination: ${url}`,
+            {
+              request,
+              user: {
+                id: cookie.userId,
+              },
+            },
+          );
+
           return reply.redirect(request.query.redirect);
         }
         logger.warn(`Redirecting to ${request.query.redirect} is not allowed`, { security: true, fail: true, request });
         return reply.redirect('/');
       } catch (err) {
         logger.error(
-          'Error logging user out',
+          '[ViewController->logout] Error logging user out.',
           {
             request,
             security: true,
@@ -162,9 +178,19 @@ module.exports = {
         );
         return reply.redirect('/');
       }
-    } else {
-      return reply.redirect('/');
     }
+
+    logger.info(
+      '[ViewController->logout] Logging user out.',
+      {
+        request,
+        user: {
+          id: cookie.userId,
+        },
+      },
+    );
+
+    return reply.redirect('/');
   },
 
   register(request, reply) {

commit 089216231f5cb8b253c92bfdb1bfaf2b11ba6446

HID-2260: prevent errors on logout
Regression introduced in 8b95025. By wiping session our variable
also got wiped. An object clone lets us log info that no longer
exists in the session store.

api/controllers/ViewController.js
@@ -3,6 +3,7 @@
  * @description Controller for pages that visitors see.
  */
 const Boom = require('@hapi/boom');
+const Hoek = require('@hapi/hoek');
 const Recaptcha = require('recaptcha2');
 const Client = require('../models/Client');
 const User = require('../models/User');
@@ -147,7 +148,7 @@ module.exports = {
 
   async logout(request, reply) {
     // Temporarily store user session for logging purposes.
-    const cookie = request.yar.get('session');
+    const oldCookie = Hoek.clone(request.yar.get('session'));
 
     // Destroy user session.
     request.yar.reset();
@@ -176,7 +177,7 @@ module.exports = {
             {
               request,
               user: {
-                id: cookie.userId,
+                id: oldCookie && oldCookie.userId,
               },
             },
           );
@@ -206,7 +207,7 @@ module.exports = {
       {
         request,
         user: {
-          id: cookie.userId,
+          id: oldCookie && oldCookie.userId,
         },
       },
     );