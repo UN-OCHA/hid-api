87fc851dbb6b462a959d70de845eb9a124332793
Chris Ruppel <ruppel@reliefweb.int>
2021-10-20 13:47:00 +0200

    chore(security): update eslint@7.32.0

--- commands/.eslintrc.js
+++ commands/.eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   rules: {
-    "no-await-in-loop": "off",
-    "no-console": "off",
-    "no-plusplus": "off",
-    "no-underscore-dangle": "off",
-    "prefer-destructuring": "off",
+    'no-await-in-loop': 'off',
+    'no-console': 'off',
+    'no-plusplus': 'off',
+    'no-underscore-dangle': 'off',
+    'prefer-destructuring': 'off',
   },
 };
