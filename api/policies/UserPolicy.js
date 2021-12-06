56bab89cf9a73ce7c4bf67b5641e18f53178bad6
Chris Ruppel <ruppel@reliefweb.int>
2021-12-06 11:05:33 +0100

    refactor: consolidate UserPolicy.canDestroy and UserPolicy.canUpdate
    They were identical after refactoring to remove deprecated code.

--- api/policies/UserPolicy.js
+++ api/policies/UserPolicy.js
@@ -8,33 +8,15 @@ const config = require('../../config/env');
 const { logger } = config;
 
 module.exports = {
-  async canDestroy(request) {
-    // If the acting user is an admin
-    //   OR any user is targeting themselves
-    // THEN are allowed to update the target user.
-    if (request.auth.credentials.is_admin || request.auth.credentials.id === request.params.id) {
-      return true;
-    }
-
-    logger.warn(
-      `[UserPolicy->canDestroy] User ${request.auth.credentials.id} can not remove user ${request.params.id}`,
-      {
-        request,
-        security: true,
-        fail: true,
-        user: {
-          id: request.auth.credentials.id,
-          email: request.auth.credentials.email,
-        },
-      },
-    );
-    throw Boom.forbidden();
-  },
-
+  /**
+   * Policy: canUpdate
+   *
+   * Any user can update themselves. Admins can update any user.
+   */
   async canUpdate(request) {
     // If the acting user is an admin
     //   OR any user is targeting themselves
-    // THEN are allowed to update the target user.
+    // THEN they are allowed to update the target user.
     if (request.auth.credentials.is_admin || request.auth.credentials.id === request.params.id) {
       return true;
     }
@@ -55,10 +37,15 @@ module.exports = {
     throw Boom.forbidden();
   },
 
+  /**
+   * Policy: canFind
+   *
+   * Any user can find themselves. Admins can find any user.
+   */
   async canFind(request) {
     // If the acting user is an admin
     //   OR any user is targeting themselves
-    // THEN are allowed to view the target user.
+    // THEN they are allowed to view the target user.
     if (request.auth.credentials.is_admin || request.auth.credentials.id === request.params.id) {
       return true;
     }
--- config/routes.js
+++ config/routes.js
@@ -398,7 +398,7 @@ module.exports = [
     path: '/api/v3/user/{id}',
     options: {
       pre: [
-        UserPolicy.canDestroy,
+        UserPolicy.canUpdate,
         AuthPolicy.isTOTPEnabledAndValid,
       ],
       handler: UserController.destroy,
