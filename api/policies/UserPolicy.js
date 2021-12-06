aa308e860d958d4283a23c4c8c4b13c6b562a108
Chris Ruppel <ruppel@reliefweb.int>
2021-12-06 10:10:47 +0100

    fix: remove HID Contacts code from UserPolicy.canDestroy

--- api/policies/UserPolicy.js
+++ api/policies/UserPolicy.js
@@ -3,28 +3,32 @@
 * @description User Policy
 */
 const Boom = require('@hapi/boom');
-const User = require('../models/User');
 const config = require('../../config/env');
 
 const { logger } = config;
 
 module.exports = {
   async canDestroy(request) {
-    if (request.auth.credentials.is_admin
-      || request.auth.credentials.id === request.params.id) {
+    // If the acting user is an admin
+    //   OR any user is targeting themselves
+    // THEN are allowed to update the target user.
+    if (request.auth.credentials.is_admin || request.auth.credentials.id === request.params.id) {
       return true;
     }
-    const user = await User.findOne({ _id: request.params.id }).populate('createdBy');
-    if (user.createdBy
-        && user.createdBy.id === request.auth.credentials.id
-        && user.email_verified === false
-        && request.auth.credentials.isManager) {
-      return true;
-    }
+
     logger.warn(
       `[UserPolicy->canDestroy] User ${request.auth.credentials.id} can not remove user ${request.params.id}`,
+      {
+        request,
+        security: true,
+        fail: true,
+        user: {
+          id: request.auth.credentials.id,
+          email: request.auth.credentials.email,
+        },
+      },
     );
-    throw Boom.unauthorized('You are not allowed to do this operation');
+    throw Boom.forbidden();
   },
 
   async canUpdate(request) {
